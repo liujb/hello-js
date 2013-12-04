782b0ab6f7ce450cb27172e4528ba1d251067edd
liujb <liujiangbei@126.com>
2013-12-04 20:52:48 +0800

    modify

---
+++ temp.js
@@ -0,0 +1,10 @@
+	var circleCfg = {
+		x: arcCfg.x - p.w,
+		y: arcCfg.y + p.h,
+		radius: 30,
+		startRadian: 0,
+		endRadian: 2,
+		lineWidth: 2,
+		color: "#ffffff",
+		strokeColor: "#e6e6e6"
+	};
